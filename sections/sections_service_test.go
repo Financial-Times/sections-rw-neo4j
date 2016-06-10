13fcda2adcbbc22ca25db6db44afb02a338f7878
Sarah Wells <sarah.wells@ft.com>
2016-06-10 11:21:57 +0100

    Write Identifiers as separate nodes, so that concordance will work

--- sections/sections_service_test.go
+++ sections/sections_service_test.go
@@ -4,100 +4,135 @@ import (
 	"os"
 	"testing"
 
-	"github.com/Financial-Times/base-ft-rw-app-go/baseftrwapp"
 	"github.com/Financial-Times/neo-utils-go/neoutils"
 	"github.com/jmcvetta/neoism"
 	"github.com/stretchr/testify/assert"
 )
 
-var sectionsDriver baseftrwapp.Service
+const (
+	sectionUUID          = "12345"
+	newSectionUUID       = "123456"
+	tmeID                = "TME_ID"
+	newTmeID             = "NEW_TME_ID"
+	prefLabel            = "Test"
+	specialCharPrefLabel = "Test 'special chars"
+)
 
-func TestDelete(t *testing.T) {
+var defaultTypes = []string{"Thing", "Concept", "Classification", "Section"}
+
+func TestCreateCompleteSectionWithPropsAndIdentifiers(t *testing.T) {
 	assert := assert.New(t)
-	uuid := "12345"
+	sectionsDriver := getSectionsCypherDriver(t)
 
-	sectionsDriver = getSectionsCypherDriver(t)
-
-	sectionToDelete := Section{UUID: uuid, CanonicalName: "Test", TmeIdentifier: "TME_ID"}
-
-	assert.NoError(sectionsDriver.Write(sectionToDelete), "Failed to write section")
-
-	found, err := sectionsDriver.Delete(uuid)
-	assert.True(found, "Didn't manage to delete section for uuid %", uuid)
-	assert.NoError(err, "Error deleting section for uuid %s", uuid)
-
-	p, found, err := sectionsDriver.Read(uuid)
-
-	assert.Equal(Section{}, p, "Found section %s who should have been deleted", p)
-	assert.False(found, "Found section for uuid %s who should have been deleted", uuid)
-	assert.NoError(err, "Error trying to find section for uuid %s", uuid)
-}
-
-func TestCreateAllValuesPresent(t *testing.T) {
-	assert := assert.New(t)
-	uuid := "12345"
-	sectionsDriver = getSectionsCypherDriver(t)
-
-	sectionToWrite := Section{UUID: uuid, CanonicalName: "Test", TmeIdentifier: "TME_ID"}
+	alternativeIds := alternativeIdentifiers{TME: []string{tmeID}, UUIDS: []string{sectionUUID}}
+	sectionToWrite := Section{UUID: sectionUUID, PrefLabel: prefLabel, AlternativeIdentifiers: alternativeIds}
 
 	assert.NoError(sectionsDriver.Write(sectionToWrite), "Failed to write section")
 
-	readSectionForUUIDAndCheckFieldsMatch(t, uuid, sectionToWrite)
+	readSectionForUUIDAndCheckFieldsMatch(assert, sectionsDriver, sectionUUID, sectionToWrite)
 
-	cleanUp(t, uuid)
+	cleanUp(assert, sectionUUID, sectionsDriver)
+}
+
+func TestCreateSectionNotAllIdentifiersPresent(t *testing.T) {
+	assert := assert.New(t)
+	sectionsDriver := getSectionsCypherDriver(t)
+
+	alternativeIds := alternativeIdentifiers{UUIDS: []string{sectionUUID}}
+	sectionToWrite := Section{UUID: sectionUUID, PrefLabel: prefLabel, AlternativeIdentifiers: alternativeIds}
+
+	assert.NoError(sectionsDriver.Write(sectionToWrite), "Failed to write section")
+
+	readSectionForUUIDAndCheckFieldsMatch(assert, sectionsDriver, sectionUUID, sectionToWrite)
+
+	cleanUp(assert, sectionUUID, sectionsDriver)
 }
 
 func TestCreateHandlesSpecialCharacters(t *testing.T) {
 	assert := assert.New(t)
-	uuid := "12345"
-	sectionsDriver = getSectionsCypherDriver(t)
+	sectionsDriver := getSectionsCypherDriver(t)
 
-	sectionToWrite := Section{UUID: uuid, CanonicalName: "Test 'special chars", TmeIdentifier: "TME_ID"}
+	alternativeIds := alternativeIdentifiers{TME: []string{tmeID}, UUIDS: []string{sectionUUID}}
+	sectionToWrite := Section{UUID: sectionUUID, PrefLabel: specialCharPrefLabel, AlternativeIdentifiers: alternativeIds}
 
 	assert.NoError(sectionsDriver.Write(sectionToWrite), "Failed to write section")
 
-	readSectionForUUIDAndCheckFieldsMatch(t, uuid, sectionToWrite)
+	readSectionForUUIDAndCheckFieldsMatch(assert, sectionsDriver, sectionUUID, sectionToWrite)
 
-	cleanUp(t, uuid)
+	cleanUp(assert, sectionUUID, sectionsDriver)
 }
 
-func TestCreateNotAllValuesPresent(t *testing.T) {
+func TestUpdateWillRemovePropertiesAndIdentifiersNoLongerPresent(t *testing.T) {
 	assert := assert.New(t)
-	uuid := "12345"
-	sectionsDriver = getSectionsCypherDriver(t)
+	sectionsDriver := getSectionsCypherDriver(t)
 
-	sectionToWrite := Section{UUID: uuid, CanonicalName: "Test"}
+	alternativeIds := alternativeIdentifiers{TME: []string{tmeID}, UUIDS: []string{sectionUUID}}
+	sectionToWrite := Section{UUID: sectionUUID, PrefLabel: prefLabel, AlternativeIdentifiers: alternativeIds}
 
 	assert.NoError(sectionsDriver.Write(sectionToWrite), "Failed to write section")
+	readSectionForUUIDAndCheckFieldsMatch(assert, sectionsDriver, sectionUUID, sectionToWrite)
 
-	readSectionForUUIDAndCheckFieldsMatch(t, uuid, sectionToWrite)
-
-	cleanUp(t, uuid)
-}
-
-func TestUpdateWillRemovePropertiesNoLongerPresent(t *testing.T) {
-	assert := assert.New(t)
-	uuid := "12345"
-	sectionsDriver = getSectionsCypherDriver(t)
-
-	sectionToWrite := Section{UUID: uuid, CanonicalName: "Test", TmeIdentifier: "TME_ID"}
-
-	assert.NoError(sectionsDriver.Write(sectionToWrite), "Failed to write section")
-	readSectionForUUIDAndCheckFieldsMatch(t, uuid, sectionToWrite)
-
-	updatedSection := Section{UUID: uuid, CanonicalName: "Test", TmeIdentifier: "TME_ID"}
+	tmeAlternativeIds := alternativeIdentifiers{UUIDS: []string{sectionUUID}}
+	updatedSection := Section{UUID: sectionUUID, PrefLabel: prefLabel, AlternativeIdentifiers: tmeAlternativeIds}
 
 	assert.NoError(sectionsDriver.Write(updatedSection), "Failed to write updated section")
-	readSectionForUUIDAndCheckFieldsMatch(t, uuid, updatedSection)
 
-	cleanUp(t, uuid)
+	readSectionForUUIDAndCheckFieldsMatch(assert, sectionsDriver, sectionUUID, updatedSection)
+
+	cleanUp(assert, sectionUUID, sectionsDriver)
 }
 
 func TestConnectivityCheck(t *testing.T) {
 	assert := assert.New(t)
-	sectionsDriver = getSectionsCypherDriver(t)
+	sectionsDriver := getSectionsCypherDriver(t)
 	err := sectionsDriver.Check()
 	assert.NoError(err, "Unexpected error on connectivity check")
+}
+
+func TestDelete(t *testing.T) {
+	assert := assert.New(t)
+	sectionsDriver := getSectionsCypherDriver(t)
+
+	alternativeIds := alternativeIdentifiers{TME: []string{tmeID}, UUIDS: []string{sectionUUID}}
+	sectionToDelete := Section{UUID: sectionUUID, PrefLabel: prefLabel, AlternativeIdentifiers: alternativeIds}
+
+	assert.NoError(sectionsDriver.Write(sectionToDelete), "Failed to write section")
+
+	found, err := sectionsDriver.Delete(sectionUUID)
+	assert.True(found, "Didn't manage to delete section for uuid %", sectionUUID)
+	assert.NoError(err, "Error deleting section for uuid %s", sectionUUID)
+
+	p, found, err := sectionsDriver.Read(sectionUUID)
+
+	assert.Equal(Section{}, p, "Found section %s who should have been deleted", p)
+	assert.False(found, "Found section for uuid %s who should have been deleted", sectionUUID)
+	assert.NoError(err, "Error trying to find section for uuid %s", sectionUUID)
+}
+
+func TestCount(t *testing.T) {
+	assert := assert.New(t)
+	sectionsDriver := getSectionsCypherDriver(t)
+
+	alternativeIds := alternativeIdentifiers{TME: []string{tmeID}, UUIDS: []string{sectionUUID}}
+	sectionOneToCount := Section{UUID: sectionUUID, PrefLabel: prefLabel, AlternativeIdentifiers: alternativeIds}
+
+	assert.NoError(sectionsDriver.Write(sectionOneToCount), "Failed to write section")
+
+	nr, err := sectionsDriver.Count()
+	assert.Equal(1, nr, "Should be 1 section in DB - count differs")
+	assert.NoError(err, "Unexpected error during count")
+
+	newAlternativeIds := alternativeIdentifiers{TME: []string{newTmeID}, UUIDS: []string{newSectionUUID}}
+	sectionTwoToCount := Section{UUID: newSectionUUID, PrefLabel: specialCharPrefLabel, AlternativeIdentifiers: newAlternativeIds}
+
+	assert.NoError(sectionsDriver.Write(sectionTwoToCount), "Failed to write section")
+
+	nr, err = sectionsDriver.Count()
+	assert.Equal(2, nr, "Should be 2 sections in DB - count differs")
+	assert.NoError(err, "Unexpected error during count")
+
+	cleanUp(assert, sectionUUID, sectionsDriver)
+	cleanUp(assert, newSectionUUID, sectionsDriver)
 }
 
 func getSectionsCypherDriver(t *testing.T) service {
@@ -112,8 +147,7 @@ func getSectionsCypherDriver(t *testing.T) service {
 	return NewCypherSectionsService(neoutils.StringerDb{db}, db)
 }
 
-func readSectionForUUIDAndCheckFieldsMatch(t *testing.T, uuid string, expectedSection Section) {
-	assert := assert.New(t)
+func readSectionForUUIDAndCheckFieldsMatch(assert *assert.Assertions, sectionsDriver service, uuid string, expectedSection Section) {
 	storedSection, found, err := sectionsDriver.Read(uuid)
 
 	assert.NoError(err, "Error finding section for uuid %s", uuid)
@@ -121,33 +155,7 @@ func readSectionForUUIDAndCheckFieldsMatch(t *testing.T, uuid string, expectedSe
 	assert.Equal(expectedSection, storedSection, "sections should be the same")
 }
 
-func TestWritePrefLabelIsAlsoWrittenAndIsEqualToName(t *testing.T) {
-	assert := assert.New(t)
-	sectionsDriver := getSectionsCypherDriver(t)
-	uuid := "12345"
-	sectionToWrite := Section{UUID: uuid, CanonicalName: "Test", TmeIdentifier: "TME_ID"}
-
-	assert.NoError(sectionsDriver.Write(sectionToWrite), "Failed to write section")
-
-	result := []struct {
-		PrefLabel string `json:"t.prefLabel"`
-	}{}
-
-	getPrefLabelQuery := &neoism.CypherQuery{
-		Statement: `
-				MATCH (t:Section {uuid:"12345"}) RETURN t.prefLabel
-				`,
-		Result: &result,
-	}
-
-	err := sectionsDriver.cypherRunner.CypherBatch([]*neoism.CypherQuery{getPrefLabelQuery})
-	assert.NoError(err)
-	assert.Equal("Test", result[0].PrefLabel, "PrefLabel should be 'Test")
-	cleanUp(t, uuid)
-}
-
-func cleanUp(t *testing.T, uuid string) {
-	assert := assert.New(t)
+func cleanUp(assert *assert.Assertions, uuid string, sectionsDriver service) {
 	found, err := sectionsDriver.Delete(uuid)
 	assert.True(found, "Didn't manage to delete section for uuid %", uuid)
 	assert.NoError(err, "Error deleting section for uuid %s", uuid)
